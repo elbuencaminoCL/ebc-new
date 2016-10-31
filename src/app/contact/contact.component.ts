540be3196bcaeefdcf3899372bd7846799bcd3d2
Jair <ejairn@gmail.com>
2016-10-31 14:22:00 -0500

    Markup forms contacto

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.styl']
+  styleUrls: ['./contact.component.styl'],
+  providers: [ NgbTabsetConfig ]
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  	constructor(config: NgbTabsetConfig) {
+		config.type = 'pills';
+    }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 
-  ngAfterViewInit() {
-    window.scrollTo(0, 0);
-  }
+	ngAfterViewInit() {
+		window.scrollTo(0, 0);
+	}
 
 }
